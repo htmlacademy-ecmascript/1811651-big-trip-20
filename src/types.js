4c6f0e62135c4dfd330180f87395b80b942c287c
Igor <trololol-lolo@mail.ru>
2023-05-16 16:01:29 +1000

    Позволяет filter-view принимать данные

--- src/types.js
+++ src/types.js
@@ -27,6 +27,23 @@
  */
 
 /**
+ * @typedef FilterViewState
+ * @prop {Array<FilterItemViewState>} items
+ */
+
+/**
+ * @typedef FilterItemViewState
+ * @prop {FilterType} value
+ * @prop {boolean} isDisabled
+ * @prop {boolean} isSelected
+ */
+
+
+/**
+ * @typedef {'everything' | 'future' | 'present' | 'past'} FilterType
+ */
+
+/**
  * @typedef ListViewState
  * @prop {Array<PointViewState>} items
  */
