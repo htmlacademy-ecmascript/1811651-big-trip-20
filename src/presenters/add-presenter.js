040b649f104e1aae33a436b5ed3569c72f2d0131
Igor <trololol-lolo@mail.ru>
2023-06-06 16:25:31 +1000

    Добавляет метод для создания "черновика" точки маршрута

--- src/presenters/add-presenter.js
+++ src/presenters/add-presenter.js
@@ -13,6 +13,25 @@ class AddPresenter extends Presenter {
       isDisabled: false
     };
   }
+
+  /**
+   * @override
+   */
+  addEventListeners() {
+    this.view.addEventListener('click', this.handleViewClick.bind(this));
+  }
+
+
+  handleViewClick() {
+    /**
+     * @type {UrlParams}
+     */
+    const urlParams = {
+      edit: 'draft'
+    };
+
+    this.setUrlParams(urlParams);
+  }
 }
 
 export default AddPresenter;
