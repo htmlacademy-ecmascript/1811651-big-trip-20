b51f7ce7545f5eb661716b2a9ee8e9f4cc876e58
Igor <trololol-lolo@mail.ru>
2023-06-06 12:38:34 +1000

    Добавляем состояния placeholder-view

--- src/views/placeholder-view.js
+++ src/views/placeholder-view.js
@@ -5,6 +5,23 @@ import {html} from '../utils.js';
  * @extends {View<PlaceholderViewState>}
  */
 class PlaceholderView extends View {
+
+  /**
+   * @override
+   */
+  createHtml() {
+    const placeholder = this.state;
+
+    if (placeholder.isHidden) {
+      return '';
+    }
+
+    return html`
+      <p class="trip-events__msg">${placeholder.text}</p>
+    `;
+  }
+}
+
 customElements.define('placeholder-view', PlaceholderView);
 
 export default PlaceholderView;
